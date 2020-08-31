20db433a9f0c3ed6dcf773efd142b60c6c6f4ec2
zastrixarundell <zastrixarundell@gmail.com>
2020-08-31 22:15:40 +0200

    Added config files

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phomenix, PhomenixWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "phomenix.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: [
+    "https://phomenix.gigalixirapp.com"
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
